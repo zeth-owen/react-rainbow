2c223262768d75cc4f8c9b0f8ee4cd6588d4b6e3
Zeth Thomas <zeththomas89@gmail.com>
2024-03-09 04:58:12 -0500

    Add New Color

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import './App.css';
+import React, { useState } from 'react'
+import ColorBlock from './ColorBlock'
+import ColorForm from './ColorForm'
+
+function App(){
+    let [colors, setColors] = useState([
+        'violet', 'blue',
+        'lightblue', 'green',
+        'greenyellow', 'yellow',
+        'orange', 'red'
+    ])
+    
+    const addColor = (newColor) => {
+        setColors([...colors, newColor])
+    }
+
+    return (
+         <div className="App">
+            {colors.map((color, i) => 
+                <ColorBlock key={i} color={color} />
+            )}
+            <ColorForm addColor={addColor}/>
+        </div>
+        
+
+)}
+
+
 
 export default App;
+
+
+
